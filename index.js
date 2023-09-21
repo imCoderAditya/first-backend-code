a1359ffee4a851dfc1c3519ce982990a674f2856
Aditya sharma <adsharmavashishtha@gmail.com>
2023-09-21 10:53:44 +0530

    commit..

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require("path");
 const app = express()
 const PORT = 4000;
+app.use(express.static(__dirname + "/public"));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 })
